a09fcdc5aba8e6c7e2203656970482e474122120
derekfrost12140 <34093825+derekfrost12140@users.noreply.github.com>
2024-04-26 13:17:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import uuid
 import tempfile
 import logging
 from ultralytics import YOLO
+import supervision as sv
 
 # Set up basic configuration for logging
 logging.basicConfig(level=logging.INFO)
@@ -33,16 +34,12 @@ def process_video():
     try:
         video_url = request.json['url']
         cap = cv2.VideoCapture(video_url)
-        
-        # Get video properties
-        frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        fps = cap.get(cv2.CAP_PROP_FPS)
-        
+        video_info = sv.VideoInfo.from_video_path(video_url)
+
         output_filename = f"{uuid.uuid4()}.mp4"
         output_path = os.path.join(tempfile.gettempdir(), output_filename)
         fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        out = cv2.VideoWriter(output_path, fourcc, fps, (frame_width, frame_height))
+        out = cv2.VideoWriter(output_path, fourcc, 20.0, (video_info.width, video_info.height))
 
         while True:
             success, img = cap.read()
@@ -51,7 +48,7 @@ def process_video():
             results = model(img, stream=True)
             processed_frame = process_frame(img, results)
             out.write(processed_frame)
-        
+       
         cap.release()
         out.release()
         return jsonify({'message': 'Processing complete', 'video': f'/videos/{output_filename}'})
