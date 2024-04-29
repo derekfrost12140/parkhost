4a4da79ed908e385cccc9c571ed4dc0634996e9f
Swami Devansh <Devanshswami@my.unt.edu>
2024-04-29 02:41:41 -0500

    changed the webpython.py

--- webpython.py
+++ webpython.py
@@ -95,13 +95,13 @@ def parking():
 class StoppableThread(Thread):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._stop_event = False
+        this._stop_event = False
 
     def stop(self):
-        self._stop_event = True
+        this._stop_event = True
 
     def stopped(self):
-        return self._stop_event
+        return this._stop_event
 
 @app.route("/select_lot", methods=["POST"])
 def select_lot():
